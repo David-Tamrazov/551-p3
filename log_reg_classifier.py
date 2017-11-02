7c3508b768b32d90cb3f4368107c9d9186a7a209
David Tamrazov <david.v.tamrazov@gmail.com>
2017-11-01 20:19:25 -0400

    Working version of Log Regression

--- log_reg_classifier.py
+++ log_reg_classifier.py
@@ -5,186 +5,120 @@ from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
-print ("Loading training and test data...") 
 
-# # Production code - uncomment for submission
-# x = np.loadtxt("train_x.csv", delimiter=",")
-# y = np.loadtxt("train_y.csv", delimiter=",")
-# x_t = np.loadtxt("test_x.csv", delimiter=",")
+def build_hyperparameter_settings():
+    reg_strengths = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
+    tol_ranges = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
 
-Y_train = []
-
-counter = 0
-with open('train_y.csv', 'r') as f: 
-
-    for line in f:
-        
-        if counter == 1000:
-            break
-        
-        idx = int(line.rstrip())
-        Y_train.append(idx)
-        counter+=1
-
-df_x = pd.read_csv("train_x.csv", nrows=1000)
-# df_y = pd.read_csv("train_y.csv", nrows=1000)
-
-x = df_x.values
-# Y_train = df_y.values
-
-print("Reshaping data...")
-
-# reshape the images to 64x64
-X_train = x.reshape(-1, 64, 64) 
-# X_test = x_t.reshape(-1, 64, 64)
-
-# reg_strengths = [1 / X_train.shape(0), 10 / X_train.shape(0), 100 / X_train.shape(0), 1000 / X_train.shape(0), 10000 / X_train.shape(0)]
-reg_strengths = [1]
-max_iterations = [10, 100, 1000, 10000, 100000]
-tol_ranges = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
-scaler = StandardScaler()
-
-print("Preprocessing the data...")
-
-# preprocess the data by subtracting the mean (centering) and scaling it down to unit size 
-for matrix in X_train:
-    scaler.fit(matrix)
-    scaler.transform(matrix)
-
-# create indices of training data and validation data for k-fold validation
-k_folds = KFold(5, shuffle=True, random_state=69).split(Y_train)
-
-c_performance = []
-max_iter_performance = []
-tol_performance = []
-
-print("Running cross validation to determine optimal regularization strength...")
-# use cross validation to find the optimal regularization strength 
-for c in reg_strengths:
-
-    # initialize empty lists to score cross validation scores 
-    validation_accuracy = [] 
-    training_accuracy = []
-
-    # iterate over k-fold indices 
-    for train_idx, val_idx in k_folds:
-
-        # training; penalty is l2 by default 
-        clf = LogisticRegression(C=c, solver='saga', max_iter=1000, tol=1e-4, n_jobs=-1)
-        clf.fit(X_train[train_idx], Y_train[train_idx])
-
-        # get predictions for measuring validation and training accuracy respectively 
-        validation_prediction = clf.predict(X_train[val_idx])
-        training_prediction = clf.predict(X_train[train_idx])
-
-        # measure and store the validation & training accuracy  
-        validation_accuracy.append((Y_train[val_idx] == validation_prediction).mean())
-        training_accuracy.append((Y_train[train_idx] == training_prediction).mean())
-
-    # average the training and validation accuracies over all folds
-    avg_train_acc = np.mean(training_accuracy)
-    avg_val_acc = np.mean(validation_accuracy)
-
-    print("C: {} | Training Accuracy: {} | Validation Accuracy: {}".format(c, avg_train_acc, avg_val_acc))
-    c_performance.append((c, avg_val_acc))
-
-# get the regularization strength that produced the highest validation accuracy 
-best_c = max(c_performance, key=lambda item:item[1])[0]
-
-print("Optimal regularization strength: {}".format(best_c))
-
-print("Running cross validation to determine optimal max iterations...")
-
-for itr in max_iterations:
+    hp_settings = []
+    for strength in reg_strengths:
+        for rng in tol_ranges:
+            hp_settings.append((strength, rng))
     
-     # initialize empty lists to score cross validation scores 
-    validation_accuracy = [] 
-    training_accuracy = []
-
-    # iterate over k-fold indices 
-    for train_idx, val_idx in k_folds:
-
-        # training; penalty is l2 by default 
-        clf = LogisticRegression(C=best_c, solver='saga', max_iter=itr, tol=1e-4, n_jobs=-1)
-        clf.fit(X_train[train_idx], Y_train[train_idx])
-
-        # get predictions for measuring validation and training accuracy respectively 
-        validation_prediction = clf.predict(X_train[val_idx])
-        training_prediction = clf.predict(X_train[train_idx])
-
-        # measure and store the validation & training accuracy  
-        validation_accuracy.append((Y_train[val_idx] == validation_prediction).mean())
-        training_accuracy.append((Y_train[train_idx] == training_prediction).mean())
-
-    # average the training and validation accuracies over all folds
-    avg_train_acc = np.mean(training_accuracy)
-    avg_val_acc = np.mean(validation_accuracy)
-
-    print("Max Iterations: {} | Training Accuracy: {} | Validation Accuracy: {}".format(itr, avg_train_acc, avg_val_acc))
-    max_iter_performance.append((itr, avg_val_acc))
-
-# keep track of the max iterator
-best_iter = max(max_iter_performance, key=lambda item:item[1])[0]
-
-print("Optimal number of max iterations: {}".format(best_iter))
-
-print("Running cross validation to determine optimal tolerance range...")
-
-for tol in tol_ranges:
+    return hp_settings
     
-     # initialize empty lists to score cross validation scores 
-    validation_accuracy = [] 
-    training_accuracy = []
 
-    # iterate over k-fold indices 
-    for train_idx, val_idx in k_folds:
+def find_best_parameters(hyperparameter_settings, training_data, training_labels):
 
-        # training; penalty is l2 by default 
-        clf = LogisticRegression(C=best_c, solver='saga', max_iter=best_iter, tol=tol, n_jobs=-1)
-        clf.fit(X_train[train_idx], Y_train[train_idx])
+    setting_performance = []
 
-        # get predictions for measuring validation and training accuracy respectively 
-        validation_prediction = clf.predict(X_train[val_idx])
-        training_prediction = clf.predict(X_train[train_idx])
+    # use cross validation to find the optimal hyperparameter setting 
+    for setting in hyperparameter_settings:
 
-        # measure and store the validation & training accuracy  
-        validation_accuracy.append((Y_train[val_idx] == validation_prediction).mean())
-        training_accuracy.append((Y_train[train_idx] == training_prediction).mean())
+        # unpack the hyperparameter setting tuple 
+        reg_strength, tol_range = setting
 
-    # average the training and validation accuracies over all folds
-    avg_train_acc = np.mean(training_accuracy)
-    avg_val_acc = np.mean(validation_accuracy)
+         # initialize empty lists to score cross validation scores 
+        validation_accuracy = [] 
+        training_accuracy = []
 
-    print("Tolerance Range: {} | Training Accuracy: {} | Validation Accuracy: {}".format(tol, avg_train_acc, avg_val_acc))
-    tol_performance.append((tol, avg_val_acc))
+        # create indices of training data and validation data for k-fold validation
+        k_folds = KFold(5, shuffle=True, random_state=69).split(training_labels)
 
-# get the regularization strength that produced the highest validation accuracy 
-best_tol = max(tol_performance, key=lambda item:item[1])[0]
+        # iterate over k-fold indices 
+        for train_idx, val_idx in k_folds:
 
-print("Optimal tolerance range: {}".format(best_tol))
+            # training; penalty is l2 by default 
+            clf = LogisticRegression(C=reg_strength, solver='sag', multi_class='multinomial', max_iter=1000, tol=tol_range, n_jobs=-1)
+            clf.fit(training_data[train_idx], training_labels[train_idx].ravel())
 
-print("Finished cross validation for all hyper parameters.")
+            # get predictions for measuring validation and training accuracy respectively 
+            validation_prediction = clf.predict(training_data[val_idx])
+            training_prediction = clf.predict(training_data[train_idx])
 
-print("Creating predictions for test data...")
+            # measure and store the validation & training accuracy  
+            validation_accuracy.append((training_labels[val_idx] == validation_prediction).mean())
+            training_accuracy.append((training_labels[train_idx] == training_prediction).mean())
 
-# Get predictions using our cross-validated hyperparameters 
-best_clf = LogisticRegression(C=best_c, solver='saga', max_iter=best_iter, tol=best_tol, n_jobs=-1)
-# test_pred = clf.predict(X_test)
+        # average the training and validation accuracies over all folds
+        avg_train_acc = np.mean(training_accuracy)
+        avg_val_acc = np.mean(validation_accuracy)
 
-print("Writing predictions to file...")
+        print("Regularization Strength: {} | Tolerance Range: {} | Training Accuracy: {} | Validation Accuracy: {}".format(setting[0], setting[1], avg_train_acc, avg_val_acc))
+        setting_performance.append((setting, avg_val_acc))
 
-# save predictions to file 
-write_to_file(test_pred)
+    # get the regularization strength that produced the highest validation accuracy 
+    best_setting = max(setting_performance, key=lambda item:item[1])[0]
 
-print("Done.")
+    print("Optimal hyper parameter setting: Regularization Strength: {}, Tolerance Range: {}".format(best_setting[0], best_setting[1]))
+
+    return best_setting
 
 
 def write_to_file(predictions):
     return 0
 
+def create_predictions(clf, test_set):
+    test_pred = clf.predict(test_set)
+
+    print("Writing predictions to file...")
+
+    # save predictions to file 
+    write_to_file(test_pred)
 
 
+def main():
     
+    print  "Loading training and test data..."  
+
+    # # Production code - uncomment for submission
+    # x = np.loadtxt("train_x.csv", delimiter=",")
+    # y = np.loadtxt("train_y.csv", delimiter=",")
+    # x_t = np.loadtxt("test_x.csv", delimiter=",")
+
+    # X = x
+    # Y_train = y
+
+    df_x = pd.read_csv("train_x.csv", nrows=1000)
+    df_y = pd.read_csv("train_y.csv", nrows=1000)
+
+    X = df_x.values
+    Y_train = df_y.values
+
+    print "Preprocessing the data..."
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X)
+
+    print "Finding the best hyperparameter setting through cross validation..."
+
+    # use cross validation to find the optimal hyperparameter setting
+    hyperparameter_settings = build_hyperparameter_settings()
+
+    best_c, best_tol = find_best_parameters(hyperparameter_settings, X_train, Y_train)
+
+    print "Finished cross validation for all hyper parameters."
+
+    print "Creating predictions for test data..."
+
+    # Get predictions using our cross-validated hyperparameters 
+    best_clf = LogisticRegression(C=best_c, solver='saga', tol=best_tol, n_jobs=-1)
+
+    # create_predictions(best_clf, X_test)
+
+    print "Done."
+
+
+main()
 
 
      
